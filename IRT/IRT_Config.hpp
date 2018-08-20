d9bd5fbbc89806c346f24a02c8d973559192dbe0
Ethan <32881589+XaLaForo@users.noreply.github.com>
2018-08-21 00:56:02 +0100

    Add files via upload

---
+++ IRT/IRT_Config.hpp
@@ -0,0 +1,8 @@
+/*
+    File: IRT_Config.hpp
+
+    Author: Ethan (XaLaForo)
+
+    Description:
+    Master include for IRT configs
+*/
---
+++ IRT/IRT_Functions.hpp
@@ -0,0 +1,8 @@
+/*
+    File: IRT_Functions.hpp
+
+    Author: Ethan (XaLaForo)
+
+    Description:
+    Master include for IRT functions
+*/
